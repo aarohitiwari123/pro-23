393be7d1b0bef9471cd11e928df621f5bf94b9c4
aarohitiwari123 <64591603+aarohitiwari123@users.noreply.github.com>
2020-05-19 15:46:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var box1,box2,box3;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800,700);
+	rectMode(CENTER);
+	
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.18
+
+	box1 = createSprite(width/2,655,123,20);
+	box2 = createSprite(335,610,20,120);
+	box3 = createSprite(458,610,20,120);
+	box1.shapeColor = "red";
+	box2.shapeColor = "red";
+	box3.shapeColor = "red";
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+	box1 = Bodies.rectangle(width/2,650,123,39, {isStatic:true} );
+	World.add(world, box1);
+	box2 = Bodies.rectangle(335,610,55,150,{isStatic:true} );
+	World.add(world, box2);
+	box3 = Bodies.rectangle(458,610,55,150,{isStatic:true} );
+	World.add(world, box3);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody,false); 
+  }
+}
+
+
+
